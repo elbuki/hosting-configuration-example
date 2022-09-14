dcf71924376fad7c8c350acaef552c9eab61134e
Marco Carmona <elbuki@users.noreply.github.com>
2022-09-14 16:44:20 -0600

    Initial Commit

---
+++ HostingConfigurationExample/HostingConfigurationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostingConfigurationExampleApp.swift
+//  HostingConfigurationExample
+//
+//  Created by Marco Carmona on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct HostingConfigurationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
